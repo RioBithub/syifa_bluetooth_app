99f28a8c9c314e643002d6bf2f30f6002149e20f
RioBithub <masterofunity@gmail.com>
2024-07-24 07:42:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bluetooth App',
+      home: BluetoothScreen(),
+    );
+  }
+}
+
+class BluetoothScreen extends StatefulWidget {
+  @override
+  _BluetoothScreenState createState() => _BluetoothScreenState();
+}
+
+class _BluetoothScreenState extends State<BluetoothScreen> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<BluetoothDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.bluetooth,
+      Permission.bluetoothScan,
+      Permission.bluetoothConnect,
+      Permission.bluetoothAdvertise,
+      Permission.location,
+    ].request();
+
+    if (statuses[Permission.bluetooth]!.isGranted &&
+        statuses[Permission.bluetoothScan]!.isGranted &&
+        statuses[Permission.bluetoothConnect]!.isGranted &&
+        statuses[Permission.bluetoothAdvertise]!.isGranted &&
+        statuses[Permission.location]!.isGranted) {
+      _startScan();
+    } else {
+      print("Some permissions are not granted");
+    }
+  }
+
+  void _startScan() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+    flutterBlue.scanResults.listen((results) {
+      for (ScanResult result in results) {
+        if (!devices.any((device) => device.id == result.device.id)) {
+          setState(() {
+            devices.add(result.device);
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bluetooth Devices (By Deva)'),
+      ),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (BuildContext context, int index) {
+          BluetoothDevice device = devices[index];
+          return ListTile(
+            title: Text(device.name.isNotEmpty ? device.name : 'Unknown device'),
+            subtitle: Text(device.id.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
